d80bbfbfe1de3a9a31e13ad04b7ef77c79c99d53
JCJARAA99 <91167214+Jaraa99@users.noreply.github.com>
2024-08-06 09:58:21 -0500

    Initial commit

---
+++ rickapi-main/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+        <footer className="bg-dark text-light text-center text-lg-start">
+            <div className="text-center p-3" >
+                
+            </div>
+            </footer>
+    )
+}
